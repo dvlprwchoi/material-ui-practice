0fbe33aaf89b53ab1d1fe2d9af8acffff34aa80b
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-03-20 16:44:14 -0500

    Add Buttons component

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+function Buttons() {
+  return (
+    <div className="buttons">
+      <h2>Buttons</h2>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default Buttons;

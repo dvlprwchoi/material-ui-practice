7c69b1ea42d5f50b5defd540d731015dcc4f2cae
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-03-20 16:44:53 -0500

    Install react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import Buttons from './components/Buttons/Buttons';
+import NavTabs from './components/NavTabs/NavTabs';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Material UI Practice</h1>
+      <NavTabs />
+      <Routes>
+        <Route path="/buttons" element={<Buttons />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

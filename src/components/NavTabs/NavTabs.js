13a2e7bc65d89de3591736c4a9aee4cf1cb310bd
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-03-20 17:51:31 -0500

    Update navtabs

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -1,38 +1,151 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
 
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
+// function LinkTab(props) {
+//   return (
+//     <Tab
+//       component="a"
+//       onClick={(event) => {
+//         event.preventDefault();
+//       }}
+//       {...props}
+//     />
+//   );
+// }
+
+// function NavTabs() {
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+//         <LinkTab label="Buttons" href="/buttons" />
+//         <LinkTab label="Page Two" href="/trash" />
+//         <LinkTab label="Page Three" href="/spam" />
+//       </Tabs>
+//     </Box>
+//   );
+// }
+
+// export default NavTabs;
+
+import * as React from 'react';
+import { styled } from '@mui/system';
+import TabsUnstyled from '@mui/base/TabsUnstyled';
+import TabsListUnstyled from '@mui/base/TabsListUnstyled';
+import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
+import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
+import Buttons from '../Buttons/Buttons';
+import Modals from '../Modals/Modals';
+import RadioButtons from '../RadioButtons/RadioButtons';
+
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#80BFFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+
+const red = {
+  50: '#fff0f0',
+  100: '#ffc2c2',
+  200: '#ff8080',
+  300: '#ff6666',
+  400: '#ff3333',
+  500: '#ff0000',
+  600: '#e50000',
+  700: '#b20000',
+  800: '#990000',
+  900: '#750000',
+};
+
+const Tab = styled(TabUnstyled)`
+  font-family: IBM Plex Sans, sans-serif;
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  padding: 12px 16px;
+  margin: 6px 6px;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${red[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    border-radius: 3px;
+    outline: 2px solid ${red[200]};
+    outline-offset: 2px;
+  }
+
+  &.${tabUnstyledClasses.selected} {
+    background-color: ${red[50]};
+    color: ${red[600]};
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(TabPanelUnstyled)`
+  width: 100%;
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+`;
+
+const TabsList = styled(TabsListUnstyled)`
+  min-width: 320px;
+  background-color: ${red[500]};
+  border-radius: 8px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+`;
 
 function NavTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
-        <LinkTab label="Buttons" href="/buttons" />
-        <LinkTab label="Page Two" href="/trash" />
-        <LinkTab label="Page Three" href="/spam" />
-      </Tabs>
-    </Box>
+    <TabsUnstyled defaultValue={0}>
+      <TabsList>
+        <Tab>Buttons</Tab>
+        <Tab>Modals</Tab>
+        <Tab>Radio Buttons</Tab>
+      </TabsList>
+      <TabPanel value={0}>
+        <Buttons />
+      </TabPanel>
+      <TabPanel value={1}>
+        <Modals />
+      </TabPanel>
+      <TabPanel value={2}>
+        <RadioButtons />
+      </TabPanel>
+    </TabsUnstyled>
   );
 }
-
 export default NavTabs;
 
 // import * as React from 'react';
